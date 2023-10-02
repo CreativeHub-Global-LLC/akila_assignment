deb406678492c0425e2bdb2415d286c61ee9f00f
= <sandini.creativehub@gmail.com>
2023-10-03 00:21:26 +0530

    dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,18 @@
 function Dashboard() {
   return (
     <div className="h-screen w-screen p-0 m-0  justify-center items-center flex">
-      <div className="w-1/6 bg-[#0052A8] justify-start items-start pl-5 flex flex-col py-10 h-full ">
-        <div className="text-white">
-          <h1 className="text-white text-3xl font-medium">
+      <div className="w-1/6 bg-[#0052A8] justify-start items-startflex flex-col py-10 h-full ">
+        <div className="text-white w-full">
+          <h1 className="text-white text-3xl font-medium  px-5">
             Ticket Reservation
           </h1>
-          <ul className="py-10 gap-10 space-y-5  font-medium">
-            <li>All Reservation</li>
-            <li>Create Reservation</li>
+          <ul className="py-10 gap-10 s  font-medium">
+            <li className="hover:bg-yellow-500 px-5 py-2 w-full duration-300">
+              All Reservation
+            </li>
+            <li className="hover:bg-yellow-500 px-5 py-2 duration-300">
+              Create Reservation
+            </li>
           </ul>
         </div>
       </div>
